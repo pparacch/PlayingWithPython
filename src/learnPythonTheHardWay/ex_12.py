a0c4c788c50fd3126102fae0bcdf3c50d724adb8
Pier Lorenzo Paracchini <pparacch.github@outlook.com>
2015-02-10 00:23:53 +0100

    Learn Python the Hard Way - exercises 11 & 12

---
+++ src/learnPythonTheHardWay/ex_12.py
@@ -0,0 +1,12 @@
+#Prompting People
+#Python version 2.7
+
+#Using 'raw_input("Prompt")'
+
+age = raw_input("How old are you? ")
+height = raw_input("How tall are you? ")
+weight = raw_input("How much do you weight? ")
+
+print "so you are %r old, %r tall and %r heavy." % (age, height, weight)
+
+
